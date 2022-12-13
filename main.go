6bd8ab2195cd900167fffb63fb6a53a83fda7fea
DavidBerlin2012 <118775412+DavidBerlin2012@users.noreply.github.com>
2022-12-13 15:25:52 +0000

    Lambdas

--- main.go
+++ main.go
@@ -4,5 +4,23 @@ import "fmt"
 
 
 func main() {
-  fmt.Println("Test")
+
+  area := func( length, width int ) int {
+    return length * width
+  }
+
+  fmt.Printf("area Type %T \n", area)
+
+  counter := func() func() int {
+    num := 0
+    return func() int {
+      num++
+      return num
+    }
+  } ()
+
+  fmt.Printf("counter Type %T \n", counter)
+  fmt.Println("Count: ", counter())
+  fmt.Println("Count: ", counter())
+
 }
