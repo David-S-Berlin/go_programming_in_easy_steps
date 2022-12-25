c9ab23b5074825f0e5c906cc1dd3d7ca94f8cf14
DavidBerlin2012 <118775412+DavidBerlin2012@users.noreply.github.com>
2022-12-25 17:49:02 +0100

    Create weekend.go

---
+++ slices/weekend.go
@@ -0,0 +1,18 @@
+package slices
+
+import fmt
+
+
+func OutputWeekend() {
+  days := [7]string {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}
+  weekend := days [5:]
+
+  fmt.Printf("Slice weekend %v \n", weekend)
+  fmt.Printf("Type weekand %T \n", weekand)
+  
+  fmt.Printf("Length weekend %v \n", len(weekend))
+  fmt.Printf("Capacity weekend %v \n", cap(weekend))
+  
+  fmt.Printf("Pointer weekend: %p \n", weekend)
+  fmt.Printf("Address days[5]: %p \n", &days[5])
+}
