3c27219bd798263527dc57a4cf8905637af2c406
DavidBerlin2012 <118775412+DavidBerlin2012@users.noreply.github.com>
2022-12-26 12:26:05 +0000

    add slice operations "append" and "remove"

--- makeslices/sliceoperations.go
+++ makeslices/sliceoperations.go
@@ -11,11 +11,21 @@ func SliceOperations() {
   more[1] = 300
   more[2] = 400
   
+  // initial state of nums
+  describe(nums)
+
+  // append a item from another slice
+  nums = append(nums, more[0])
+  describe(nums)
+
+  // remove the first item in "nums"
+  copy(nums[0:], nums[1:])
+  nums = nums[: len(nums)-1]
   describe(nums)
 
 }
 
 func describe(nums []int) {
-  fmt.Printf("\n%v Length: %v", nums, len(nums))
+  fmt.Printf("\n%v Length: %v ", nums, len(nums))
   fmt.Printf("Capacity: %v", cap(nums))
 }
