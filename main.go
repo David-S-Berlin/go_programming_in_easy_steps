71eed15668f9657ee6a1e43258722607bb1524f0
DavidBerlin2012 <118775412+DavidBerlin2012@users.noreply.github.com>
2022-12-26 13:01:48 +0100

    Update main.go

--- main.go
+++ main.go
@@ -33,26 +33,5 @@ func main() {
 }
 
 
-func closure() {
-
-  area := func( length, width int ) int {
-    return length * width
-  }
-
-  fmt.Printf("area Type %T \n", area)
-
-  counter := func() func() int {
-    num := 0
-    return func() int {
-      num++
-      return num
-    }
-  } ()
-
-  fmt.Printf("counter Type %T \n", counter)
-  fmt.Println("Count: ", counter())
-  fmt.Println("Count: ", counter())
-}
-
 
 
