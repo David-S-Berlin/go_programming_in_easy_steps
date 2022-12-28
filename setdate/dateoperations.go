e17763cef571a68db4f543193dca2dcd79749606
DavidBerlin2012 <118775412+DavidBerlin2012@users.noreply.github.com>
2022-12-28 10:56:42 +0100

    Create dateoperations.go

---
+++ setdate/dateoperations.go
@@ -0,0 +1,20 @@
+package setdate
+
+import (
+  "fmt"
+  "time"
+)
+
+
+func DateOperations() {
+
+  
+  // create own Date
+  dt := time.Date(2025, time.January, 1, 12, 0, 0, time.Local)
+  fmt.Printf("\nDateTime: %v \n\n", dt)
+  
+  // AddDate
+  dt = dt.AddDate(2, 6, 3)
+  fmt.Printf("Added DateTime: %v \n\n")
+
+}
