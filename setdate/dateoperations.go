e838c5e1169dba0fb1dcaff6d2bbf5920781f320
DavidBerlin2012 <118775412+DavidBerlin2012@users.noreply.github.com>
2022-12-28 18:11:10 +0000

    add time.Parse

--- setdate/dateoperations.go
+++ setdate/dateoperations.go
@@ -1,19 +1,29 @@
 package setdate
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 )
-
 
 func DateOperations() {
 
-  // create own Date
-  dt := time.Date(2025, time.January, 1, 12, 0, 0, 0, time.Local)
-  fmt.Printf("\nDateTime: %v \n\n", dt)
-  
-  // AddDate
-  dt = dt.AddDate(2, 6, 3)
-  fmt.Printf("Added DateTime: %v \n\n", dt)
+	// create own Date
+	dt := time.Date(2025, time.January, 1, 12, 0, 0, 0, time.Local)
+	fmt.Printf("\n\nDateTime: %v \n\n", dt)
+
+	// AddDate
+	dt = dt.AddDate(2, 6, 3)
+	fmt.Printf("Added DateTime: %v \n\n", dt)
+
+	// own layout
+	layout := "2006-Jan-02 03:04PM"
+	str := "2030-Dec-25 12:30AM"
+
+	t, err := time.Parse(layout, str)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Parsed Time %v \n", t)
+	}
 
 }
